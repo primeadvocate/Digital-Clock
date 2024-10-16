030b71074c5b0fd0933489901cffe039bf1256be
Olayinka <olagokeolayinka@gmail.com>
2024-10-16 17:03:10 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let hrs = document.getElementById('hrs');
+let min = document.getElementById('min');
+let secs = document.getElementById('sec');
+
+setInterval(() => {
+   let currentTime = new Date()
+
+    hrs.innerHTML = (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHours()
+    min.innerHTML =
+    (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes()
+    secs.innerHTML = (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds() 
+}, 1000)
